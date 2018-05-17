2bfbda03371059398288a746cb87e1c5ff004f73
Joseph Weber <joseph.theodore.weber@gmail.com>
2018-05-17 15:27:32 -0400

    created game logic

--- JS/main.js
+++ JS/main.js
@@ -1,6 +1,16 @@
-var cardOne = "queen";
-var cardTwo = "king";
-var cardThree = "queen";
-var cardFour = "king";
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardFour);
+var cards = ["queen", "queen", "king", "king"];
+var cardOne = cards[0];
+var cardTwo = cards[2];
+var cardThree = cards[1];
+var cardFour = cards[3];
+var cardsInPlay = [];
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardThree);
+console.log("User flipped queen")
+console.log("User flipped king")
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1])
+{ alert("You found a match!")}
+else {
+	alert("Sorry, try again");
+} }
