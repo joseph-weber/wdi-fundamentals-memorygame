62828ca57cc3b9f796416c54b1c0b8823d780bcc
Joseph Weber <joseph.theodore.weber@gmail.com>
2018-05-18 12:21:38 -0400

    created game logic

--- JS/main.js
+++ JS/main.js
@@ -1,16 +1,15 @@
 var cards = ["queen", "queen", "king", "king"];
-var cardOne = cards[0];
-var cardTwo = cards[2];
-var cardThree = cards[1];
-var cardFour = cards[3];
 var cardsInPlay = [];
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardThree);
-console.log("User flipped queen")
-console.log("User flipped king")
-if (cardsInPlay.length === 2) {
-	if (cardsInPlay[0] === cardsInPlay[1])
-{ alert("You found a match!")}
-else {
-	alert("Sorry, try again");
-} }
+var checkForMatch = function () {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+ 	alert("You found a match!")}
+ 	 else {
+	alert("Sorry, try again.");
+	} };
+var flipCard = function (cardId) {
+	cardsInPlay.push(cards[cardId])
+	console.log("User flipped " + cards[cardId])
+	if (cardsInPlay.length === 2)
+	checkForMatch()};
+	flipCard(0);
+	flipCard(1);
