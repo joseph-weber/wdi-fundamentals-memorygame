c2b60a912ea10d23c6dd8b3dcc1694c8f0c7e567
Joseph Weber <joseph.theodore.weber@gmail.com>
2018-05-18 14:44:27 -0400

    created game logic

--- JS/main.js
+++ JS/main.js
@@ -1,4 +1,25 @@
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+{
+rank: 'queen',
+suit: 'hearts',
+image: "images/queen-of-hearts.png"
+},
+{
+rank: 'queen',
+suit: 'diamonds',
+image: "images/queen-of-diamonds.png"
+},
+{
+rank: 'king',
+suit: 'hearts',
+image: "images/king-of-hearts.png"
+},
+{
+rank: 'king',
+suit: 'diamonds',
+image: "images/king-of-diamonds.png"
+}
+];
 var cardsInPlay = [];
 var checkForMatch = function () {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
@@ -7,9 +28,11 @@ var checkForMatch = function () {
 	alert("Sorry, try again.");
 	} };
 var flipCard = function (cardId) {
-	cardsInPlay.push(cards[cardId])
-	console.log("User flipped " + cards[cardId])
+	cardsInPlay.push(cards[cardId].rank)
+	console.log("User flipped " + cards[cardId].rank)
+	console.log("User flipped " + cards[cardId].image)
+	console.log("User flipped " + cards[cardId].suit)
 	if (cardsInPlay.length === 2)
-	checkForMatch()};
+	{checkForMatch()}};
 	flipCard(0);
 	flipCard(1);
